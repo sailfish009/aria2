595521e466f71d17b1e6da33ebf9fa08f512d309
sailfish009 <sailfish009@gmail.com>
2020-09-04 10:44:02 +0900

    Create aria2.py

---
+++ aria2.py
@@ -0,0 +1,12 @@
+class Aria2(nn.Module):
+    """
+    ARiA2 activation function, a special case of ARiA, for ARiA = f(x, 1, 0, 1, 1, b, 1/a)
+    """
+
+    def __init__(self, a=1.5, b = 2.):
+        super(Aria2, self).__init__()
+        self.a = a
+        self.b = b
+
+    def forward(self, x):
+        return x* ((1 + torch.exp(-x)**self.b)**(-self.a))
